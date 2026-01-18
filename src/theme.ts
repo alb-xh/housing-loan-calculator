0eee56558fcec356a2cb0284e1eebc73a2b8c9fb
albxh <albano.xhafaj10@gmail.com>
2026-01-18 01:12:38 +0100

    update metadata

--- src/theme.ts
+++ src/theme.ts
@@ -4,4 +4,4 @@ import type { Theme } from "./contexts/theme-context";
 
 export const isDarkTheme = () => globalThis?.document?.documentElement?.classList?.contains('dark');
 export const isLightTheme = () => !isDarkTheme();
-export const getDefaultTheme = (): Theme => isDarkTheme() ? 'dark' : 'light';
+export const getDefaultTheme = (): Theme => isLightTheme() ? 'light' : 'dark';
